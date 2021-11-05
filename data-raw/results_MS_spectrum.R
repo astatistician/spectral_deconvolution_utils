f6c2cabe38ff2adf7cde9a019245182ab8888969
Piotr Prostko <piotr.prostko@uhasselt.be>
2021-11-05 14:05:40 +0100

    Include all charge states in the "results_MS_spectrum" dataset.

--- data-raw/results_MS_spectrum.R
+++ data-raw/results_MS_spectrum.R
@@ -1,6 +1,6 @@
 ## code to prepare `results_MS_spectrum` dataset goes here
 library(tidyverse)
-results_MS_spectrum <- read.csv2(here::here("extdata/results_MS_spectrum.csv")) %>%
+results_MS_spectrum <- read.csv(here::here("inst/extdata/results_MS_spectrum.csv")) %>%
   select(-X) %>%
   as.data.frame()
 usethis::use_data(results_MS_spectrum, overwrite = TRUE)
