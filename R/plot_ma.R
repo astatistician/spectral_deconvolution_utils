ccbf872b1572e3853fc3a3c930f59ab986e9cfd7
Piotr Prostko <piotr.prostko@uhasselt.be>
2021-11-05 15:34:42 +0100

    Modify plot_ma(): input data in the long format; pass additional mappings like colour etc. through ...

--- R/plot_ma.R
+++ R/plot_ma.R
@@ -1,8 +1,10 @@
 #' MA plot of estimates vs ground truth
 #'
-#' @param x numeric, the ground truth (spiked) proportion/ratio values.
-#' @param y numeric, estimated mixing proportions/ratios.
-#' @param ... expression(s) (don't use ” or "", just quote or rlang::expr) that
+#' @param data data Data in longer format containing the mapping variables.
+#' @param x Bare variable name of the ground truth, i.e. spiked-in proportion.
+#' @param y Bare variable name of the estimated proportions.
+#' @param ... Pass additional mappings to \code{ggplot} like colour, symbol, size etc. using bare variable names.
+#' @param extra_ellipsis expression(s) (don't use ” or "", just quote or rlang::expr) that
 #'   will be evaluated in order to modify the plot (e.g. add new geom, change
 #'   axis labels etc.).
 #'
@@ -13,16 +15,17 @@
 #'   library(tidyverse)
 #' }
 #' dat <- results_MS_spectrum %>%
-#'   filter(z == 9 & spectrum_type == "imp") %>%
-#'   distinct(ground_truth, scaling_factor)
-#' plot_ma(x = dat$ground_truth, y = dat$scaling_factor)
-plot_ma <- function(x, y, ...) {
-  gg_dat <- bind_cols(x = x, y = y) %>%
+#'   filter(spectrum_type == "imp") %>%
+#'   distinct(z, ground_truth, scaling_factor)
+#' plot_ma(dat, x = ground_truth, y = scaling_factor, colour = factor(z))
+plot_ma <- function(data, x, y, ..., extra_ellipsis = NULL) {
+  gg_dat <- data %>%
     tidyr::drop_na() %>%
-    filter(x > 0 & y > 0) %>%
-    mutate(`log2Ratio` = log2(y / x), `log2Avg` = .5 * (log2(x) + log2(y)))
+    filter({{x}} > 0 & {{y}} > 0) %>%
+    mutate(`log2Ratio` = log2({{y}} / {{x}}),
+           `log2Avg` = .5 * (log2({{x}}) + log2({{y}})))
   p <- gg_dat %>%
-    ggplot(aes(x = `log2Avg`, y = `log2Ratio`)) +
+    ggplot(aes(x = `log2Avg`, y = `log2Ratio`, ...)) +
     geom_point() +
     geom_hline(yintercept = 0.0, color = "black") +
     geom_hline(yintercept = 1.0, color = "black", linetype = "dotted") +
@@ -30,9 +33,10 @@ plot_ma <- function(x, y, ...) {
     scale_x_continuous(breaks = pretty_breaks(n = 8)) +
     scale_y_continuous(breaks = pretty_breaks(n = 8)) +
     theme_bw()
-  add_options <- list(...)
-  for (i in seq_along(add_options)) {
-    p <- p + eval(add_options[[i]])
+  if (!is.null(extra_ellipsis)) {
+    for (i in seq_along(extra_ellipsis)) {
+      p <- p + eval(extra_ellipsis[[i]])
+    }
   }
   p
 }
