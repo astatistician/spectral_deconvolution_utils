27a8c23f1566cae53ba417f8c1a06c6f1193d1f3
Piotr Prostko <piotr.prostko@uhasselt.be>
2021-11-12 22:37:57 +0100

    Read in ssNMR data in long format tibble instead of using list.

--- R/read_spectrum_nmr.R
+++ R/read_spectrum_nmr.R
@@ -1,16 +1,18 @@
 #' Read in NMR spectrum
 #'
-#' Vectorized function to read in multiple NMR spectra (acquired using Bruker's equipment) at once.
+#' Vectorized function to read in multiple NMR spectra (acquired using Bruker's
+#' equipment) at once.
 #'
-#' @param path If type = "spectrum", path should point at the folder with "1r" and "1i" files.
-#' If type = "fid", the path should be the folder with the "fid" file. type = "text" can not be handled currently.
-#' @param type A character vector controlling how the data is loaded, see the \emph{path} argument.
+#' @param path If type = "spectrum", path should point at the folder with "1r"
+#'   and "1i" files. If type = "fid", the path should be the folder with the
+#'   "fid" file. type = "text" can not be handled currently.
+#' @param type A character vector controlling how the data is loaded, see the
+#'   \emph{path} argument.
 #'
-#' @return List with two elements \itemize{
-#'   \item \emph{data}: another list of a size corresponding to the number of loaded spectra.
-#'   Each element is a complex vector with the name attribute that reflects spectrum's ppm values.
-#'   \item \emph{info}: matrix with acquisition information (OFFSET, SW_p, SF, SI, BYTORDP, NC_proc, FTSIZE). One spectrum per row.
-#' }
+#' @return List with two elements \itemize{ \item \emph{data}: a tibble in long
+#'   format with three variables: signal - signal id, x - ppm value, y - complex
+#'   signal. \item \emph{info}: matrix with acquisition information (signal id,
+#'   OFFSET, SW_p, SF, SI, BYTORDP, NC_proc, FTSIZE). One spectrum per row. }
 #' @export
 #'
 #' @examples
@@ -23,6 +25,7 @@ read_spectrum_nmr <- function(path, type) {
   type <- match.arg(type, c("spectrum", "fid", "text"))
 
   n_files <- length(path)
+  info_out <- NULL
   data_out <- vector("list", n_files)
 
   if (type == "fid") {
@@ -58,11 +61,8 @@ read_spectrum_nmr <- function(path, type) {
         imaginary = fid[seq(from = 2, to = info_out[i, "TD"], by = 2)]
       )
       time_vec <- seq(0, (length(fid) - 1) * info_out[i, "DW"], by = info_out[i, "DW"])
-      names(fid) <- time_vec
-      data_out[[i]] <- fid
+      data_out[[i]] <- tibble(x = time_vec, y = fid)
     }
-
-    return(list(data = data_out, info = info_out))
   } else if (type == "spectrum") {
     params <- c("OFFSET", "SW_p", "SF", "SI", "BYTORDP", "NC_proc", "FTSIZE")
     info_out <- matrix(nrow = n_files, ncol = length(params))
@@ -96,16 +96,15 @@ read_spectrum_nmr <- function(path, type) {
         size = 4L, endian = "little"
       )
       spec <- complex(real = spec_r, imaginary = spec_i)
-      names(spec) <- ppm
-      data_out[[i]] <- spec
+      data_out[[i]] <- tibble(x = ppm, y = spec)
     }
-    return(list(data = data_out, info = info_out))
   } else if (type == "text") {
     for (i in 1:n_files) {
       tmp_spec <- utils::read.table(path[i], sep = " ", header = FALSE)
-      data_out[[i]] <- tmp_spec[, 2]
-      names(data_out[[i]]) <- tmp_spec[, 1]
+      data_out[[i]] <- tibble(x = tmp_spec[, 1], y = tmp_spec[, 2])
     }
-    return(list(data = data_out))
   }
+  data_out <- set_names(data_out, paste0("signal", 1 : n_files))
+  return(list(data = bind_rows(data_out, .id = "signal"),
+              info = bind_cols(signal = paste0("signal", 1:n_files), info_out)))
 }
